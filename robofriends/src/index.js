4b06f24edd28b8eed95b1d22487596605ca64fd8
stephenunder <41934385+stephenunder@users.noreply.github.com>
2019-01-21 17:31:15 -0600

    react app part 3

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,13 +1,13 @@
+import './index.css';
+import "tachyons";
+
+import * as serviceWorker from './serviceWorker';
+
+import App from "./App";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import CardList from "./CardList";
-import * as serviceWorker from './serviceWorker';
-import "tachyons";
-import { robots } from "./robots";
 
-ReactDOM.render(<CardList robots={robots} />, 
-  document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
