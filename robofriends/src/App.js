93ca9fd5e0795d323b904f8af7a4d35bbcee9c39
stephenunder <41934385+stephenunder@users.noreply.github.com>
2019-01-21 17:46:27 -0600

    style react app

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,3 +1,5 @@
+import "./App.css";
+
 import React, { Component } from "react";
 
 import CardList from "./CardList";
@@ -23,7 +25,7 @@ class App extends Component {
     })
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
