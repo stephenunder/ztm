824cdd5e4b9a0188d4145a76dfc4878b8aff5cb6
stephenunder <41934385+stephenunder@users.noreply.github.com>
2019-02-11 22:31:51 -0600

    integrate db with server via knex

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
+const knex = require("knex")
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "stephen",
+    password: "12345",
+    database: "smart-brain"
+  }
+});
 
 const app = express();
 
@@ -43,14 +54,17 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  });
-  res.json(database.users[database.users.length - 1]);
+  db("users")
+    .returning("*")
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0]);
+    })
+    .catch(err => res.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
